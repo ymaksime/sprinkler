ae38c86710224334d420e23d427f1bcddb9d6c57
Yevgeniy <m.yevgeniy.s@gmail.com>
2021-05-12 22:35:03 -0700

    Added LED control to the valve class

--- valve.py
+++ valve.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
-from gpiozero import OutputDevice
+from gpiozero import OutputDevice, LED
 
 valve_logger = logging.getLogger('Valve')
 
@@ -15,22 +15,26 @@ class Valve:
             {'model': 'CP075', 'brand': 'RainBird', 'thread': 0.75, 'acVolt': 24, 'mAcurrent': 250},
             {'model': 'CPF075', 'brand': 'RainBird', 'thread': 0.75, 'acVolt': 24, 'mAcurrent': 300}
             ]
-    def __init__(self, model, gpioPort, name):
+    def __init__(self, model, valveGpioPort, ledGpioPort, name):
         self.specifications = next((specs for specs in self.models if specs["model"] == model), None)
         assert self.specifications != None, "No such sprinkler model exist"
-        self.line = OutputDevice(gpioPort)
+        self.line = OutputDevice(valveGpioPort)
+        self.led = LED(ledGpioPort)
         self.name = name
         valve_logger.debug('Created %s valve', self.name)
+        valve_logger.debug('valve gpio: %s, led gpio: %s', self.line, self.led)
         self.info()
 
     def on(self):
         """Turns ON this valve"""
-        valve_logger.info('Turned ON the %s valve', self.name)
         self.line.on()
+        self.led.on()
+        valve_logger.info('Turned ON the %s valve', self.name)
 
     def off(self):
         """Turns OFF this valve"""
         self.line.off()
+        self.led.off()
         valve_logger.info('Turned OFF the %s valve', self.name)
 
     def is_active(self):
